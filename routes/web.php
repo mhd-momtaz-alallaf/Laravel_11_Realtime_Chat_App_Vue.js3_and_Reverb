81ae9ac154d652580efa07b9dc950a0ce937dd49
mhd-momtaz-alallaf <parental.co122@gmail.com>
2024-07-08 08:27:24 +0300

    Passing all the Not logged in users to the dashboard view

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -8,7 +9,10 @@ Route::get('/', function () {
 
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return view('dashboard',[
+        // passing all not logged in users to the dashboard view.
+        'users' => User::whereNot('id', auth()->id())->get(),
+    ]);
 })->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
