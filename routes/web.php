24adaa2eb93186494859855193e91231a9c30846
mhd-momtaz-alallaf <parental.co122@gmail.com>
2024-07-08 10:16:36 +0300

    Creating the messages route in the web.php file to get all the sender and receiver messages

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\ChatMessage;
 use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
@@ -21,5 +22,26 @@ Route::get('/chat/{friend}', function(User $friend){
         'friend' => $friend,
     ]);
 })->middleware('auth')->name('chat');
+
+// Creating the messages route and getting all the sender and receiver messages to use it later in the ChatComponent as data api.
+Route::get('messages/{friend}',function(User $friend){
+    return ChatMessage::withRelations()->query()
+        // The messages sent by the authenticated user to the friend.
+        ->where(function($query) use ($friend){
+            $query
+            ->where('sender_id', auth()->id())
+            ->where('receiver_id', $friend->id);
+        })
+        // The messages sent by the friend to the authenticated user.
+        ->orWhere(function($query) use ($friend){
+            $query
+            ->where('sender_id', $friend->id)
+            ->where('receiver_id',auth()->id());
+        })
+        // sorting the messages by the message id.
+        ->orderBy('id', 'asc')
+        // getting the query results.
+        ->get();
+})->middleware('auth');
 
 require __DIR__.'/auth.php';
