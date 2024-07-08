40b721ab205e6b05db5604814c04b3d3ba4a1a31
mhd-momtaz-alallaf <parental.co122@gmail.com>
2024-07-08 21:50:14 +0300

    Implementing the base chat layout in the ChatComponent.vue file

--- themes/tailwind/js/components/ChatComponent.vue
+++ themes/tailwind/js/components/ChatComponent.vue
@@ -1,23 +1,60 @@
 <template>
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Example Component</div>
+    <div>
+        <!-- messages aria -->
+        <div class="flex flex-col justify-end h-80">
+            <div class="p-4 overflow-y-auto max-h-fit">
+                <div v-for="message in messages" :key="message.id" class="flex items-center mb-2">
 
-                    <div class="card-body">
-                        I'm an example component.
+                    <div v-if="message.isMe" class="p-2 ml-auto text-white bg-blue-500 rounded-lg">
+                        {{ message.text }}
+                    </div>
+
+                    <div v-else class="p-2 mr-auto bg-gray-200 rounded-lg">
+                        {{ message.text }}
                     </div>
                 </div>
             </div>
         </div>
+
+        <!-- the text messages input and send button -->
+        <div class="flex items-center">
+            <input
+                type="text"
+                v-model="newMessage"
+                @keydown="sendTypingEvent"
+                @keyup.enter="sendMessage"
+                placeholder="Type a message..."
+                class="flex-1 px-2 py-1 border rounded-lg"
+            />
+
+            <button @click="sendMessage" class="px-4 py-1 ml-2 text-white bg-blue-500 rounded-lg">
+                Send
+            </button>
+        </div>
     </div>
 </template>
 
-<script>
-    export default {
-        mounted() {
-            console.log('Component mounted.')
+<script setup>
+    import { ref } from 'vue';
+
+    const messages= ref([
+        { id: 1, text: 'Hello', isMe: false },
+        { id: 2, text: 'hi', isMe: true },
+    ]);
+
+    const newMessage= ref("");
+
+    const sendMessage = () => {
+        if (newMessage.value.trim() !== "") {
+            messages.value.push({
+                id: messages.value.length + 1,
+
+                text: newMessage.value,
+
+                isMe: true,
+            });
+
+            newMessage.value = "";
         }
-    }
+    };
 </script>
