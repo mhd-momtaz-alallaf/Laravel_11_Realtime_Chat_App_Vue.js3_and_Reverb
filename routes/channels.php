5db55384280da34a9d987694c28dbae24f9c9303
mhd-momtaz-alallaf <parental.co122@gmail.com>
2024-07-09 20:04:34 +0300

    Defining the channel broadcast route that used inside the MessageSent Event

--- routes/channels.php
+++ routes/channels.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Broadcast;
 
-Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
+// defining the channel broadcast route to use it inside the MessageSent Event.
+Broadcast::channel('chat.{id}', function ($user, $id) {
+    // if the current logged in user ($user) id is the same of receiver($id) id, then he is going to be authenticated and he will receive the private message via the PrivateChannel that inside the MessageSent Event.
     return (int) $user->id === (int) $id;
 });
