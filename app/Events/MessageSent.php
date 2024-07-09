7ac9ee19f8b8365f17fa1d21ff1eecce98241b02
mhd-momtaz-alallaf <parental.co122@gmail.com>
2024-07-09 20:03:20 +0300

    Implementing the MessageSent Event to broadcast the message to the receiver

--- app/Events/MessageSent.php
+++ app/Events/MessageSent.php
@@ -2,22 +2,26 @@
 
 namespace App\Events;
 
+use App\Models\ChatMessage;
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Broadcasting\PresenceChannel;
 use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class MessageSent
+class MessageSent implements ShouldBroadcastNow
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
     /**
      * Create a new event instance.
      */
-    public function __construct()
+
+    // passing the ChatMessage Model instance(the message instance) to the constructor, to get the access to the sent message to broadcasting it immediately to the receiver.
+    public function __construct(public ChatMessage $message)
     {
         //
     }
@@ -30,7 +34,9 @@ class MessageSent
     public function broadcastOn(): array
     {
         return [
-            new PrivateChannel('channel-name'),
+            // broadcasting the message to the receiver(immediately after sending it by the sender).
+            // the used the route have been defined in routes/channels.php
+            new PrivateChannel("chat.{$this->message->receiver_id}"),
         ];
     }
 }
