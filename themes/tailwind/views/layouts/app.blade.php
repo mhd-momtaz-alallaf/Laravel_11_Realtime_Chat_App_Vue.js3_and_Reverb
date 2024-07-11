444a6be5b87026e88267f61998483cfcbc2320f5
mhd-momtaz-alallaf <parental.co122@gmail.com>
2024-07-11 22:28:04 +0300

    Modifying the app.blade.php padding

--- themes/tailwind/views/layouts/app.blade.php
+++ themes/tailwind/views/layouts/app.blade.php
@@ -18,7 +18,7 @@
 
             <!-- Page Heading -->
             <header class="bg-white shadow">
-                <div class="px-4 py-6 mx-auto max-w-7xl sm:px-6 lg:px-8">
+                <div class="px-4 pt-4 pb-1 mx-auto max-w-7xl sm:px-6 lg:px-8">
                     {{ $header }}
                 </div>
             </header>
